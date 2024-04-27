73a99fd5de6d4898eecd52e48a137f7a38516274
BuildThingsWithPKR <157862225+BuildThingsWithPKR@users.noreply.github.com>
2024-04-27 07:34:58 +0530

    Update plots.py

--- Week3/plots.py
+++ Week3/plots.py
@@ -5,13 +5,6 @@ Created on Sat Apr 27 06:32:58 2024
 @author: PKumars'
 """
 
-'''
-# Numpy
-# MatplotLib
-# Latex
-Requires latex installed for latex interpretation of legends
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -21,11 +14,11 @@ plt.rcParams.update(plt.rcParamsDefault)
 
 n = np.linspace(1, 30, num=300)
 
-plt.plot(np.log(n), np.log(8*n),label=r'$log(8n)$')
-plt.plot(np.log(n), np.log(4*n*np.log(n)),label=r'$4nlog(n)$')
-plt.plot(np.log(n), np.log(2*n*n),label=r'$log(2n^2)$')
-plt.plot(np.log(n), np.log(n*n*n),label=r'$log(n^3)$')
-plt.plot(np.log(n), np.log(pow(2,n)),label=r'$log(2^n)$')
+plt.plot(np.log2(n), np.log2(8*n),label=r'$log2(8n)$')
+plt.plot(np.log2(n), np.log2(4*n*np.log2(n)),label=r'$4nlog2(n)$')
+plt.plot(np.log2(n), np.log2(2*n*n),label=r'$log2(2n^2)$')
+plt.plot(np.log2(n), np.log2(n*n*n),label=r'$log2(n^3)$')
+plt.plot(np.log2(n), np.log2(pow(2,n)),label=r'$log2(2^n)$')
 
 plt.xlabel("log(n)") 
 plt.ylabel("log(f(n))") 
